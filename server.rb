53842717515034c1e4f16ba9ab510da946333fb6
Paul Brescia <brescia717@gmail.com>
2014-09-02 13:57:47 -0400

    Everything currently working.

--- server.rb
+++ server.rb
@@ -3,16 +3,6 @@ require 'sinatra/reloader'
 require 'pg'
 require 'pry'
 
-# def db_connection(sql)
-#   log = []
-#   begin
-#     connection = PG.connect(dbname: 'recipes')
-#     log = connection.exec(sql).to_a
-#   ensure
-#     connection.close
-#   end
-# end
-
 def db_connection
   begin
     connection = PG.connect(dbname: 'recipes')
@@ -46,43 +36,3 @@ get '/recipes/:id' do
 
   erb :recipes_id
 end
-
-
-
-
-# get '/recipes' do
-#   @recipe_names = []
-#   @recipe_ids = []
-#   recipes = 'SELECT recipes.name AS name, recipes.id AS id FROM recipes ORDER BY name' # JOIN ingredients ON ingredients.recipe_id = recipes.id ORDER BY recipes.name ASC'
-
-#   db_connection(recipes).each do |recipe|
-#       @recipe_names << recipe["name"]
-#     @recipe_names = @recipe_names.uniq
-#       @recipe_ids << recipe["id"]
-#     @recipe_ids = @recipe_ids.uniq
-#   end
-#   @recipe_names
-#   @recipe_ids
-
-#   erb :recipes
-# end
-
-# get '/recipes/:id' do
-#   info = 'SELECT recipes.name AS name, recipes.id AS id, recipes.description AS description, recipes.instructions AS instructions FROM recipes LEFT OUTER JOIN ingredients ON ingredients.recipe_id = recipes.id WHERE recipes.id = $1'
-#   @recipe_info = []
-#   @recipe_info = db_connection(info).find_all do |ids|
-#     ids["id"] == params[:id]
-
-#   end
-#   @recipe_info
-
-
-#   @ingredients = []
-#   ingredients = 'SELECT DISTINCT ingredients.name, ingredients.recipe_id FROM ingredients INNER JOIN recipes ON recipes.id = ingredients.recipe_id'
-#   @ingredients = db_connection(ingredients).find_all do |recipe|
-#     recipe["recipe_id"] == params[:id]
-#   end
-#   @ingredients = @ingredients.uniq
-
-#   erb :recipes_id
-# end
