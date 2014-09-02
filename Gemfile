1c7a199acf2adea981eebea58fde91e3f51ab7d9
Paul Brescia <brescia717@gmail.com>
2014-09-02 13:38:37 -0400

    Currently working files.

--- Gemfile
+++ Gemfile
@@ -4,15 +4,3 @@ gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'pg'
 gem 'pry'
-
-
-<html>
-<body>
-  <% @i = 0 %>
-
-  <% @recipes.each do |recipe| %>
-      <p><a href="/recipes/<%=@recipes_id[@i]%>"><%= recipe %></a></p>
-    <% @i += 1 %>
-  <% end %>
-</body>
-</html>
--- server.rb
+++ server.rb
@@ -3,11 +3,22 @@ require 'sinatra/reloader'
 require 'pg'
 require 'pry'
 
-def db_connection(sql)
-  log = []
+# def db_connection(sql)
+#   log = []
+#   begin
+#     connection = PG.connect(dbname: 'recipes')
+#     log = connection.exec(sql).to_a
+#   ensure
+#     connection.close
+#   end
+# end
+
+def db_connection
   begin
     connection = PG.connect(dbname: 'recipes')
-    log = connection.exec(sql).to_a
+
+    yield(connection)
+
   ensure
     connection.close
   end
@@ -15,36 +26,63 @@ end
 
 
 get '/recipes' do
-  @recipe_names = []
-  @recipe_ids = []
-  sql = 'SELECT * FROM ingredients JOIN recipes ON ingredients.recipe_id = recipes.id ORDER BY recipes.name ASC'
+  query = 'SELECT recipes.name AS name, recipes.id AS id FROM recipes ORDER BY recipes.name'
 
-  db_connection(sql).each do |recipe|
-    @recipe_names << recipe["name"]
-    @recipe_ids << recipe["recipe_id"].to_i
+  @recipe_names = db_connection do |conn|
+    conn.exec(query)
   end
-  @recipe_names = @recipe_names.uniq
-  @recipe_ids = @recipe_ids.uniq
 
   erb :recipes
 end
 
 get '/recipes/:id' do
-  sql = 'SELECT DISTINCT recipes.name, recipes.id FROM recipes JOIN ingredients ON recipes.id = ingredients.recipe_id ORDER BY recipes.name ASC'
-  @recipe_info = []
-  @recipe_info = db_connection(sql).find_all do |ids|
-    ids["id"] == params[:id]
+  selected = params[:id]
+  query = 'SELECT recipes.name AS name, recipes.description AS description, recipes.instructions AS instructions,
+   ingredients.name AS ingredients FROM recipes LEFT OUTER JOIN ingredients ON ingredients.recipe_id = recipes.id WHERE recipes.id = $1'
 
+  @recipe_info = db_connection do |conn|
+    conn.exec(query, [selected])
   end
-  @recipe_info
-
-
-  @ingredients = []
-  sqll = 'SELECT DISTINCT ingredients.name, ingredients.recipe_id FROM ingredients JOIN recipes ON recipes.id = ingredients.recipe_id'
-  @ingredients = db_connection(sqll).find_all do |recipe|
-    recipe["recipe_id"] == params[:id]
-  end
-  @ingredients = @ingredients.uniq
 
   erb :recipes_id
 end
+
+
+
+
+# get '/recipes' do
+#   @recipe_names = []
+#   @recipe_ids = []
+#   recipes = 'SELECT recipes.name AS name, recipes.id AS id FROM recipes ORDER BY name' # JOIN ingredients ON ingredients.recipe_id = recipes.id ORDER BY recipes.name ASC'
+
+#   db_connection(recipes).each do |recipe|
+#       @recipe_names << recipe["name"]
+#     @recipe_names = @recipe_names.uniq
+#       @recipe_ids << recipe["id"]
+#     @recipe_ids = @recipe_ids.uniq
+#   end
+#   @recipe_names
+#   @recipe_ids
+
+#   erb :recipes
+# end
+
+# get '/recipes/:id' do
+#   info = 'SELECT recipes.name AS name, recipes.id AS id, recipes.description AS description, recipes.instructions AS instructions FROM recipes LEFT OUTER JOIN ingredients ON ingredients.recipe_id = recipes.id WHERE recipes.id = $1'
+#   @recipe_info = []
+#   @recipe_info = db_connection(info).find_all do |ids|
+#     ids["id"] == params[:id]
+
+#   end
+#   @recipe_info
+
+
+#   @ingredients = []
+#   ingredients = 'SELECT DISTINCT ingredients.name, ingredients.recipe_id FROM ingredients INNER JOIN recipes ON recipes.id = ingredients.recipe_id'
+#   @ingredients = db_connection(ingredients).find_all do |recipe|
+#     recipe["recipe_id"] == params[:id]
+#   end
+#   @ingredients = @ingredients.uniq
+
+#   erb :recipes_id
+# end
